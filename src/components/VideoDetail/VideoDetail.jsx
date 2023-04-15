5c1ad004ef14c61af0d9951e96f14396d4d5c784
antu <antusaha970@gmail.com>
2023-04-16 02:09:54 +0600

    added video details page

--- src/components/VideoDetail/VideoDetail.jsx
+++ src/components/VideoDetail/VideoDetail.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import { Box, Stack } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import ReactPlayer from "react-player";
+import { useParams } from "react-router-dom";
+import { fetchFromAPI } from "../../utils/API/fetchFromAPI";
 
 const VideoDetail = () => {
-  return <div>video detail</div>;
+  const { id } = useParams();
+  const [videoDetails, setVideoDetails] = useState(null);
+  useEffect(() => {
+    fetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then((data) => {
+      setVideoDetails(data.items[0]);
+    });
+  }, []);
+  console.log(videoDetails);
+  return (
+    <Box minHeight="95vh">
+      <Stack direction={{ xs: "column", md: "row" }}>
+        <Box flex={1}>
+          <Box
+            sx={{
+              width: "100%",
+              position: "sticky",
+              top: "86px",
+            }}
+          >
+            <ReactPlayer
+              url={`https://www.youtube.com/watch?v=${id}`}
+              controls
+              className="react-player"
+            />
+          </Box>
+        </Box>
+      </Stack>
+    </Box>
+  );
 };
 
 export default VideoDetail;
