f4c7b574b4825b377a39a483f2657a211266a944
antu <antusaha970@gmail.com>
2023-04-16 01:10:45 +0600

    added search feed functionaliy

--- src/components/SearchFeed/SearchFeed.jsx
+++ src/components/SearchFeed/SearchFeed.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import { Box, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { fetchFromAPI } from "../../utils/API/fetchFromAPI";
+import { Videos } from "../";
+import { useParams } from "react-router-dom";
 
 const SearchFeed = () => {
-  return <div>Search Feed</div>;
+  const { searchTerm } = useParams();
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) => {
+      setVideos(data.items);
+    });
+  }, [searchTerm]);
+  return (
+    <Box
+      p={2}
+      sx={{
+        color: "white",
+        overflowY: "auto",
+        height: "90vh",
+        flex: 2,
+      }}
+    >
+      <Typography variant="h4" fontWeight="bold" mb={2}>
+        <span>{searchTerm} </span>
+        <span
+          style={{
+            color: "#f31503",
+          }}
+        >
+          Videos
+        </span>
+      </Typography>
+
+      <Videos videos={videos} />
+    </Box>
+  );
 };
 
 export default SearchFeed;
