f711c81165d6c62df8bd561719f3782232af4bcf
antu <antusaha970@gmail.com>
2023-04-17 20:13:13 +0600

    added comment feed

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -17,6 +17,7 @@ export const logo = "https://i.ibb.co/s9Qys2j/logo.png";
 export const categories = [
   { name: "New", icon: <HomeIcon /> },
   { name: "Jhankar Mahabub", icon: <CodeIcon /> },
+  { name: "CodeWithHarry ", icon: <CodeIcon /> },
   { name: "Coding", icon: <CodeIcon /> },
   { name: "ReactJS", icon: <CodeIcon /> },
   { name: "NextJS", icon: <CodeIcon /> },
